7e01395485855ea801850c333fecc53a27006a28
Anna Petrova <petrovanna91@mail.ru>
2023-01-05 01:38:35 +0300

    Add getFileName()

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import axios from 'axios';
-// import fsp from 'fs/promises';
+import fsp from 'fs/promises';
+import path from 'path';
 
-const pageLoader = (url /* dir = process.cwd() */) => {
-  const result = axios.get(url)
-    .then((response) => console.log(response));
-  return result;
+const getFileName = (url) => {
+  const nameWithoutProtocol = url.split('://')[1];
+  const newName = nameWithoutProtocol.replace(/[^a-z0-9]/gm, '-');
+  return `${newName}.html`;
+};
+
+const pageLoader = (url, dir = process.cwd()) => {
+  const nameFile = getFileName(url);
+  const fullFilePath = path.join(dir, nameFile);
+
+  return axios.get(url)
+    .then((response) => fsp.writeFile(fullFilePath, response.data))
+    .then(() => console.log(fullFilePath));
 };
 export default pageLoader;
