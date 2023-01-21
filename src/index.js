ef435b2ebeb6b4a32e5fe16efb6f8f191a8d8455
Anna Petrova <petrovanna91@mail.ru>
2023-01-21 23:46:37 +0300

    Refactor pageLoader(), getFileName(), downloadResources()

--- src/index.js
+++ src/index.js
@@ -2,62 +2,64 @@ import axios from 'axios';
 import fsp from 'fs/promises';
 import path from 'path';
 import cheerio from 'cheerio';
-// import url from 'node:url';
 
-const getFileName = (url2) => {
-  const nameWithoutProtocol = url2.split('://')[1];
-  const newName = nameWithoutProtocol.replace(/[^a-z0-9]/gm, '-');
-  return `${newName}.html`;
+const getFileName = (url2, originUrl) => {
+  const url = new URL(url2, originUrl);
+  const { hostname, pathname } = url;
+  const extension = path.extname(pathname);
+  // console.log('extension:', extension);
+
+  const pathName = extension ? pathname.split(extension)[0] : pathname;
+  // console.log('pathName:', pathName);
+
+  const newName = `${hostname}${pathName}`.replace(/[^a-z0-9]/gm, '-');
+  // console.log('newNamet:', newName);
+
+  const fullNewName = extension ? `${newName}${extension}` : `${newName}.html`;
+  // console.log('fullNewName:', fullNewName);
+
+  return fullNewName;
 };
 
-const downloadResources = (html, url3, dirPath) => {
+const mapping = [
+  { tag: 'img', attribute: 'src' },
+  { tag: 'link', attribute: 'href' },
+  { tag: 'script', attribute: 'src' },
+];
+
+const downloadResources = (html, dirPath, dirN, fullPath, originUrl) => {
   const $ = cheerio.load(html);
-  const names = [];
 
-  const originUrl = new URL(url3, url3);
-  const { origin } = originUrl;
+  mapping.map(({ tag, attribute }) => $(tag).each((_index, el) => {
+    const elem = $(el).attr(attribute);
+    // console.log('elem', elem); //
+    const url = new URL(elem, originUrl);
+    const { href, origin } = url;
 
-  $('img').each((_index, el) => {
-    const elem = $(el).attr('src');
-    const myUrl2 = new URL(elem, origin);
-    const { href, pathname, hostname } = myUrl2;
-
-    const extension = path.extname(pathname);
-    const urlWithoutExt = pathname.split(extension)[0];
-    const newName = `${urlWithoutExt.replace(/[^a-z0-9]/gm, '-')}${extension}`;
-
-    // console.log(path.join(dirPath, `${hostname}${newName}`));
-    names.push(newName);
-    return axios.get(href, { responseType: 'arraybuffer' })
-      .then((response) => fsp.writeFile(path.join(dirPath, `${hostname}${newName}`), response.data));
-  });
-  return names;
+    if (origin === originUrl && elem !== undefined) {
+      const newName = getFileName(elem, originUrl);
+      return axios.get(href, { responseType: 'arraybuffer' })
+        .then((response) => fsp.writeFile(path.join(dirPath, newName), response.data))
+        .then(() => {
+          $(el).attr(attribute, path.join(dirN, newName));
+          const newFile = $.html();
+          return fsp.writeFile(fullPath, newFile);
+        });
+    }
+    return null;
+  }));
 };
-
-/* const changeLinks = (file, imageNames, dirName, hostName) => {
-  const $ = cheerio.load(file);
-  $('img').each((_index, el) => () => {
-    const names = [];
-    const elem = $(el).attr('src', path.join(dirName, `${hostName}${imageNames}`));
-    names.push(elem);
-
-    return names;
-  });
-  const newFile = $.html();
-  return fsp.writeFile(fullHtmlPath, newFile);
-}; */
 
 const pageLoader = (url1, dir = process.cwd()) => {
-  const nameHtml = getFileName(url1);
-  const fullHtmlPath = path.join(dir, nameHtml);
-  const dirName = nameHtml.replace('.html', '_files');
-  const fullDirPath = path.join(dir, dirName);
-
   const parseUrl = new URL(url1, url1);
-  const { hostname } = parseUrl;
-  const hostName = hostname.replace(/[^a-z0-9]/gm, '-');
+  const { origin } = parseUrl;
 
-  let imgNames;
+  // const nameHtml = `${getFileName(url1, origin)}.html`;
+  const nameHtml = getFileName(url1, origin);
+  const fullHtmlPath = path.join(dir, nameHtml);
+  // const dirName = `${getFileName(url1, origin)}_files`;
+  const dirName = getFileName(url1, origin).replace('.html', '_files');
+  const fullDirPath = path.join(dir, dirName);
 
   return axios.get(url1, {
     responseType: 'arraybuffer',
@@ -66,25 +68,7 @@ const pageLoader = (url1, dir = process.cwd()) => {
     .then(() => fsp.access(fullDirPath))
     .catch(() => fsp.mkdir(fullDirPath))
     .then(() => fsp.readFile(fullHtmlPath, 'utf-8'))
-    .then((file) => downloadResources(file, url1, fullDirPath)) // получаю массив ссылок на картинки
-    .then((imgData) => {
-      imgNames = imgData;
-      // console.log(imgNames);
-    })
-    .then(() => fsp.readFile(fullHtmlPath, 'utf-8'))
-    .then((file) => {
-      const $ = cheerio.load(file);
-      $('img').each((_index, el) => {
-        const names = [];
-        let i = 0;
-        const elem = $(el).attr('src', path.join(dirName, `${hostName}${imgNames[i]}`));
-        names.push(elem);
-        i += 1;
-        return names;
-      });
-      const newFile = $.html();
-      return fsp.writeFile(fullHtmlPath, newFile);
-    })
+    .then((file) => downloadResources(file, fullDirPath, dirName, fullHtmlPath, origin))
     .then(() => console.log(fullHtmlPath));
 };
 export default pageLoader;
