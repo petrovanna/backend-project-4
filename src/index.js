ed5f794eb8a1f4118c040b3b45f09fe8ce4d61a4
Anna Petrova <petrovanna91@mail.ru>
2023-01-23 21:17:25 +0300

    Add import downloadResources and getFile Name

--- src/index.js
+++ src/index.js
@@ -1,64 +1,13 @@
 import axios from 'axios';
 import fsp from 'fs/promises';
 import path from 'path';
-import cheerio from 'cheerio';
 
-const getFileName = (url2, originUrl) => {
-  const url = new URL(url2, originUrl);
-  const { hostname, pathname } = url;
-  const extension = path.extname(pathname);
-  // console.log('extension:', extension);
-
-  const pathName = extension ? pathname.split(extension)[0] : pathname;
-  // console.log('pathName:', pathName);
-
-  const newName = `${hostname}${pathName}`.replace(/[^a-z0-9]/gm, '-');
-  // console.log('newNamet:', newName);
-
-  const fullNewName = extension ? `${newName}${extension}` : `${newName}.html`;
-  // console.log('fullNewName:', fullNewName);
-
-  return fullNewName;
-};
-
-const mapping = [
-  { tag: 'img', attribute: 'src' },
-  { tag: 'link', attribute: 'href' },
-  { tag: 'script', attribute: 'src' },
-];
-
-const downloadResources = (html, dirPath, dirN, fullPath, originUrl) => {
-  const $ = cheerio.load(html);
-
-  const promises = mapping.map(({ tag, attribute }) => $(tag).each((_index, el) => {
-    const elem = $(el).attr(attribute);
-    // console.log('elem', elem); //
-
-    const url = new URL(elem, originUrl);
-    const { href, origin } = url;
-
-    if (origin === originUrl && elem !== undefined) {
-      const newName = getFileName(elem, originUrl);
-
-      return axios.get(href, { responseType: 'arraybuffer' })
-        .then((response) => fsp.writeFile(path.join(dirPath, newName), response.data))
-        .then(() => {
-          $(el).attr(attribute, path.join(dirN, newName));
-          const newFile = $.html();
-          return fsp.writeFile(fullPath, newFile);
-        });
-    }
-    return null;
-  }));
-
-  const promise = Promise.all(promises);
-  return promise;
-};
+import getFileName from './get_file_name.js';
+import downloadResources from './download_resources.js';
 
 const pageLoader = (url1, dir = process.cwd()) => {
   const parseUrl = new URL(url1, url1);
   const { origin } = parseUrl;
-
   // const nameHtml = `${getFileName(url1, origin)}.html`;
   const nameHtml = getFileName(url1, origin);
   const fullHtmlPath = path.join(dir, nameHtml);
