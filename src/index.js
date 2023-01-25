dde17c20f74d47de52d1d65d9ac5e043c37cd88f
Anna Petrova <petrovanna91@mail.ru>
2023-01-26 02:40:57 +0300

    Add friendly message: Page was successfully downloaded into

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import 'axios-debug-log';
 import axios from 'axios';
 import fsp from 'fs/promises';
 import path from 'path';
@@ -5,13 +6,15 @@ import path from 'path';
 import getFileName from './get_file_name.js';
 import downloadResources from './download_resources.js';
 
+// const log = debug('page-loader');
+
 const pageLoader = (url1, dir = process.cwd()) => {
   const parseUrl = new URL(url1, url1);
   const { origin } = parseUrl;
-  // const nameHtml = `${getFileName(url1, origin)}.html`;
+
   const nameHtml = getFileName(url1, origin);
   const fullHtmlPath = path.join(dir, nameHtml);
-  // const dirName = `${getFileName(url1, origin)}_files`;
+
   const dirName = getFileName(url1, origin).replace('.html', '_files');
   const fullDirPath = path.join(dir, dirName);
 
@@ -23,11 +26,12 @@ const pageLoader = (url1, dir = process.cwd()) => {
     .catch(() => fsp.mkdir(fullDirPath))
     .then(() => fsp.readFile(fullHtmlPath, 'utf-8'))
     .then((file) => downloadResources(file, fullDirPath, dirName, fullHtmlPath, origin))
-    .then(() => console.log(fullHtmlPath));
+    .then(() => console.log(`Page was successfully downloaded into '${fullHtmlPath}'`));
 };
 export default pageLoader;
 
-// page-loader --output /var/tmp https://ru.hexlet.io/courses;
-// page-loader https://ru.hexlet.io/courses;
+// page-loader --output /var/tmp https://ru.hexlet.io/courses
+// page-loader --output /var/tmp https://page-loader.hexlet.repl.co/
+// page-loader https://ru.hexlet.io/courses
 // https://cdn2.hexlet.io/assets/logo_ru-495f05850e0095ea722a2b583565d492719579c02b0ce61d924e4f895fabf781.svg
 // https://cdn2.hexlet.io/assets/at_a_laptop-8c6e59267f91a6bf13bae0e5c0f7e1f36accc440b8d760bca08ab244e2b8bdbf.png
